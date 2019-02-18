a6c56f31a2b7213612d0d078dafaa2726be0ca34
olma <olma@10.153.1.1>
2019-02-18 16:28:36 +0100

    test

---
+++ learningPhyton/learningEx.py
@@ -0,0 +1,18 @@
+print('Hello, Olga')
+print('yes - another Hello, World!')
+print(2+1)
+
+print(2+5)
+print(3/2)
+print(7%4)
+print(2**3) #2 la puterea a 3a
+my_income = 300
+
+tax_rate = 0.15
+tax_value = my_income * tax_rate
+print(tax_value)
+newStr = "Hello"
+second = "Olga"
+print(newStr + ", "+second)
+print(newStr.index("o"))
+print("teste test")
