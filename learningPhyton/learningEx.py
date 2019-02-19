ebd56ad005f703c0f0212d02a3d56953cc601091
olma <olma@10.153.1.1>
2019-02-19 11:11:45 +0100

    numbers and strings

--- learningPhyton/learningEx.py
+++ learningPhyton/learningEx.py
@@ -11,8 +11,44 @@ my_income = 300
 tax_rate = 0.15
 tax_value = my_income * tax_rate
 print(tax_value)
-newStr = "Hello"
+newStr = "Hellom"
 second = "Olga"
 print(newStr + ", "+second)
-print(newStr.index("o"))
-print("teste test")
+print(newStr.index("o"))#index of letter o -> 4, from Hello
+print(newStr[4])#return letter that is at index 4 -> o
+print(newStr[-1])#reverse index -1 is associated with the last letter/index if the word becomes Hellom => -1 will return "m" and not o
+print("teste \ntest")# \n is for new line
+print("teste \ttest")# \n is for TAB
+print(len(newStr))#length of a string
+testString = "abcdefghijk"
+print(testString[2:])#substring from char at index 2 untill end
+print(testString[:3])#substr from the begining until index 3, not including index 3
+print(testString[2:6])#substring from index 2 until, not including, index 6
+print(testString[::2])#substring from index 0 until the end, but jumping from 0 +2 elements => acegik
+print(testString[::3])#substring from index 0 until the end, but jumping from 0 +3 elements => adgj
+print(testString[2:7:2])#substring from index 2 until index 7, but jumping from 2 +2 elements => ceg
+print(testString[::-1])#reverse string
+anotherString = 'Z' + newStr[1:]#cancat 2 strings
+print(anotherString)
+s = "Hi this is a string"
+print(s.split()) #the split() made by space 9default) and returns a list
+print(s.split("i")) #splits the string s by letter i
+print("this is new {}".format("inserted")) #string format
+print("The {} {} {}".format("fox", "brown", "quick"))
+print("The {2} {1} {0}".format("fox", "brown", "quick"))
+print("The {2} {2} {2}".format("fox", "brown", "quick"))
+print("The {q} {b} {f}".format(f="fox", b="brown", q="quick"))
+result = 100/777
+print("The result was {r}".format(r=result))
+print("The result is {r:1.3f}".format(r=result)) #formats the width and decimals after comma
+name = "Jose"
+print("Hello, {}".format(name))
+print(f"Hello, {name}") #same thing different
+age=5
+print(f"{name} is {age} years old")
+
+
+
+
+
+
