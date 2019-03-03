d483d03e716a834de95d106b19ae82fe314c751c
olga <git>
2019-03-03 17:55:28 +0100

    challenges

--- learningPhyton/learningEx.py
+++ learningPhyton/learningEx.py
@@ -197,6 +197,10 @@ for (a,b) in myList:
     print(a)
     print(b)
 
+#second going through tuples from the list
+for a in myList:
+    print(a[0])
+    
 #for loop for dictionaries
 d={'key1':1,'key2':5,'key3':9}
 #go through keys
@@ -447,6 +451,146 @@ def strfunc(inputString):
     return newString
 print(strfunc('Anthropomorphism'))
 
+'''LESSER OF TWO EVENS: Write a function that returns the lesser of two given numbers if both numbers are even, but returns the greater if one or both numbers are odd
+lesser_of_two_evens(2,4) --> 2
+lesser_of_two_evens(2,5) --> 5
+'''
+def lesserOfTwoEvens(a,b):
+    if a%2==0 and b%2==0:
+        print(min(a,b))
+        return min(a,b)
+    else:
+        print(max(a,b))
+        return max(a,b)
+lesserOfTwoEvens(8, 2)
 
+def animalCrackers(string):
+    myList=string.split()
+    for item in range(0, len(myList)):
+        for nextItem in range(1, len(myList)):
+            print(myList[item][0] )
+            print(myList[nextItem][0])
+            if myList[item][0] == myList[nextItem][0]:
+                print(True)
+                return True
+            else:
+                print(False)
+                return False
+animalCrackers('String Otring')
 
+def makes_twenty(n1,n2):
+    if (n1+n2==20) or (n1==20) or (n2==20):
+        print(True)
+        return True
+    else:
+        print(False)
+        return False
+makes_twenty(2, 8)
 
+''' Write a function that capitalizes the first and fourth letters of a name '''
+def old_macdonald(name):
+    newString = name[:3].capitalize() + name[3:].capitalize()
+    print(newString)
+    return newString
+old_macdonald('macdonald')
+
+''' Given a sentence, return a sentence with the words reversed
+master_yoda('I am home') --> 'home am I'
+master_yoda('We are ready') --> 'ready are We' '''
+def master_yoda(text):
+    list=text.split()
+    print(list)
+    newString=''
+    #the long way, without ::-1
+    
+    for item in range(len(list)-1, -1, -1):
+        if(item == len(list)-1):
+            newString=list[item]
+        else:
+            newString=newString+' '+list[item]
+    print(newString)
+    return newString
+#the short way
+'''    newList = list[::-1]
+    newString = ' '.join(newList) -> joins the elements from the list with space between them
+    print(newString)
+    return newString'''
+master_yoda('I am home')
+
+''' FIND 33:
+Given a list of ints, return True if the array contains a 3 next to a 3 somewhere.
+
+has_33([1, 3, 3]) → True
+has_33([1, 3, 1, 3]) → False
+has_33([3, 1, 3]) → False'''
+def has_33(num):
+    for item in range(0, len(num)):
+        if num[item] == 3:
+            for nextItem in range(item+1, len(num)):
+                if(num[item] == num[nextItem]):
+                    print(True)
+                    return True
+                else:
+                    print(False)
+                    return False
+
+has_33([3,1,3])
+
+'''Given a string, return a string where for every character in the original there are three characters
+paper_doll('Hello') --> 'HHHeeellllllooo'
+paper_doll('Mississippi') --> 'MMMiiissssssiiippppppiii' '''
+def paper_doll(text):
+    newString=''
+    for letter in text:
+        newString=newString+(letter*3)    
+    print(newString)
+    return newString
+paper_doll('Hello')
+
+''' Given three integers between 1 and 11, if their sum is less than or equal to 21, return their sum. 
+If their sum exceeds 21 and there's an eleven, reduce the total sum by 10. 
+Finally, if the sum (even after adjustment) exceeds 21, return 'BUST'
+blackjack(5,6,7) --> 18
+blackjack(9,9,9) --> 'BUST'
+blackjack(9,9,11) --> 19'''
+def blackjack(a,b,c):
+    sum=a+b+c
+    
+    if(a==11 or b==11 or c==11):
+        sum=sum-10
+    
+    if sum > 21:
+        print('BUST')
+        return 'BUST'      
+    
+    print(sum)    
+    return sum
+blackjack(9,9,11)
+
+''' Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 9 (every 6 will be followed by at least one 9). Return 0 for no numbers.
+summer_69([1, 3, 5]) --> 9
+summer_69([4, 5, 6, 7, 8, 9]) --> 9
+summer_69([2, 1, 6, 9, 11]) --> 14 '''
+def summer_69(arr):
+    indexSix = 0
+    indexNine = 0
+    sum=0
+    for index, value in enumerate(arr):
+        if value == 6:
+            indexSix=index
+            firstList=arr[:indexSix]
+            print(firstList)
+
+        elif value == 9:
+            indexNine=index
+            if(indexNine < len(arr)-1):
+                secondList=arr[indexNine+1:]
+            else:
+                secondList=[]
+                print(secondList)
+    for item in firstList+ secondList:
+        sum=sum+item
+    print(sum)
+    return sum
+summer_69([4,5,6,7,8,9])
+        
