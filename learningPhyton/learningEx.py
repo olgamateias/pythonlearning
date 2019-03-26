772ad04d6650ad7011850cd1fc025a1c2abf7c3b
olma <olma@10.153.1.63>
2019-03-26 17:23:52 +0100

    reg exp (continue)

--- learningPhyton/learningEx.py
+++ learningPhyton/learningEx.py
@@ -982,3 +982,35 @@ test_patterns = [ 'sd*',     # s followed by zero or more d's
                 ]
 
 multi_re_find(test_patterns,test_phrase)
+print('-----------------------------------')
+test_phrase = 'sdsd..sssddd...sdddsddd...dsds...dsssss...sdddd'
+
+test_patterns = ['[sd]',    # either s or d
+                's[sd]+']   # s followed by one or more s or d
+
+multi_re_find(test_patterns,test_phrase)
+print('-----------------------------------')
+test_phrase = 'This is a string! But it has punctuation. How can we remove it?'
+print(re.findall('[^!.?]+', test_phrase))
+print('-----------------------------------')
+
+test_phrase = 'This is an example sentence. Lets see if we can find some letters.'
+
+test_patterns=['[a-z]+',      # sequences of lower case letters
+               '[A-Z]+',      # sequences of upper case letters
+               '[a-zA-Z]+',   # sequences of lower or upper case letters
+               '[A-Z][a-z]+'] # one upper case letter followed by lower case letters
+                
+multi_re_find(test_patterns,test_phrase)
+print('-----------------------------------')
+test_phrase = 'This is a string with some numbers 1233 and a symbol #hashtag'
+
+test_patterns=[ r'\d+', # sequence of digits
+                r'\D+', # sequence of non-digits
+                r'\s+', # sequence of whitespace
+                r'\S+', # sequence of non-whitespace
+                r'\w+', # alphanumeric characters
+                r'\W+', # non-alphanumeric
+                ]
+
+multi_re_find(test_patterns,test_phrase)
